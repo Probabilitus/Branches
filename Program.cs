89b16e3b161b1c4b165a491be5426351c400ae13
Diamantine <meteoritus75@gmail.com>
2024-05-08 12:50:28 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Fruits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> fruits = new List<string>()
+            {
+                "pommes",
+                "baanane",
+                "fraises",
+
+            };
+            foreach (string fruit in fruits)
+            {
+                Console.WriteLine(fruit);
+            }
+
+            
+            
+        }
+    }
+}
